46ac069356ac76233087ffb1c756a9a2707074d3
z9j1c <ergycjc@gmail.com>
2020-04-22 12:51:36 +0300

    Char modifier

--- x86-64/zprintf/test.c
+++ x86-64/zprintf/test.c
@@ -2,6 +2,6 @@
 extern int zprintf(const char *format, ...);
 
 int main(int argc, char** argv) {
-    zprintf("help%d~~~%d", 2513, 1137);
+    zprintf("~~~%s%s~~~%d~~%c~%c~%x~~%b", "Go ", "to the hell", 512, 'A', '^', 32, 65);
     return 0;
 }
